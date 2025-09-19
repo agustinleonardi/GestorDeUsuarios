ee4175b42b2320e35f0fd428a369c7eaf29c53c7
agustinleonardi <agustinleonardi5@gmail.com>
2025-09-19 01:38:49 -0300

    Migrations y conexion bdd

--- src/GestorDeUsuarios.API/Program.cs
+++ src/GestorDeUsuarios.API/Program.cs
@@ -1,12 +1,36 @@
+using GestorDeUsuarios.Domain.Abstracciones.Repositorios;
+using GestorDeUsuarios.Infrastructure.Data;
+using GestorDeUsuarios.Infrastructure.Mappings;
+using GestorDeUsuarios.Infrastructure.Repositorios;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotNetEnv.Env.Load("../../.env");
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+    throw new InvalidOperationException("No se encontró la variable DB_CONNECTION_STRING.");
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IDomicilioRepository, DomicilioRepository>();
+builder.Services.AddAutoMapper(typeof(DomainToEntityProfile).Assembly);
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
